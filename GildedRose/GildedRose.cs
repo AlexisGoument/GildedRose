3b724982fe0b45aae1953ada1680e31b46598d73
AlexisGoument <alexis.nguyen.goument@gmail.com>
2025-04-07 17:14:56 +0200

    solution

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata;
+
+public class GildedRose
+{
+    private readonly IList<Item> Items;
+
+    public GildedRose(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+        foreach (var item in Items)
+        {
+            switch (GetItemType(item))
+            {
+                case ItemType.Common:
+                    UpdateCommonItem(item);
+                    break;
+                case ItemType.AgedBrie:
+                    UpdateAgedBrieItem(item);
+                    break;
+                case ItemType.Sulfuras:
+                    continue;
+                case ItemType.BackstagePasses:
+                    UpdateBackstagePassesItem(item);
+                    break;
+            }
+
+            if (item.Quality < 0) item.Quality = 0;
+            if (item.Quality > 50) item.Quality = 50;
+        }
+    }
+
+    private void UpdateCommonItem(Item item)
+    {
+        item.SellIn--;
+        item.Quality--;
+        if (item.SellIn < 0) item.Quality--;
+    }
+
+    private void UpdateAgedBrieItem(Item item)
+    {
+        item.SellIn--;
+        item.Quality++;
+        if (item.SellIn < 0) item.Quality++;
+    }
+
+    private void UpdateBackstagePassesItem(Item item)
+    {
+        if (item.SellIn > 10) item.Quality++;
+        else if (item.SellIn > 5) item.Quality += 2;
+        else if (item.SellIn > 0) item.Quality += 3;
+        else item.Quality = 0;
+
+        item.SellIn--;
+    }
+
+    private ItemType GetItemType(Item item)
+    {
+        var itemName = item.Name;
+        if (itemName.StartsWith("Conjured "))
+            itemName = itemName.Substring(0, 9);
+
+        if (itemName.StartsWith("Sulfuras")) return ItemType.Sulfuras;
+        if (itemName.StartsWith("Backstage passes")) return ItemType.BackstagePasses;
+        if (itemName == "Aged Brie") return ItemType.AgedBrie;
+
+        return ItemType.Common;
+    }
+}
+
+internal enum ItemType
+{
+    Common,
+    AgedBrie,
+    BackstagePasses,
+    Sulfuras
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Test]
+    public void UpdateQuality_WithCommonItem_ShouldDecreaseQuality()
+    {
+        var items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("+5 Dexterity Vest"));
+        Assert.That(items[0].SellIn, Is.EqualTo(9));
+        Assert.That(items[0].Quality, Is.EqualTo(19));
+    }
+
+    [Test]
+    public void UpdateQuality_WithCommonItem_ShouldNotDecreaseQualityBeyond0()
+    {
+        var items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("+5 Dexterity Vest"));
+        Assert.That(items[0].SellIn, Is.EqualTo(9));
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
+
+    // [Test]
+    // public void UpdateQuality_WithConjuredCommonItem_ShouldDecreaseQualityTwice()
+    // {
+    //     var items = new List<Item> { new() { Name = "Conjured +5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+    //     var app = new GildedRose(items);
+    //
+    //     app.UpdateQuality();
+    //
+    //     Assert.That(items[0].Name, Is.EqualTo("Conjured +5 Dexterity Vest"));
+    //     Assert.That(items[0].SellIn, Is.EqualTo(9));
+    //     Assert.That(items[0].Quality, Is.EqualTo(18));
+    // }
+
+    [Test]
+    public void UpdateQuality_WithCommonItemWhenNegativeSellIn_ShouldDecreaseQualityTwice()
+    {
+        var items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("+5 Dexterity Vest"));
+        Assert.That(items[0].SellIn, Is.EqualTo(-1));
+        Assert.That(items[0].Quality, Is.EqualTo(18));
+    }
+
+    [Test]
+    public void UpdateQuality_WithAgedBrie_ShouldIncreaseQuality()
+    {
+        var items = new List<Item> { new() { Name = "Aged Brie", SellIn = 10, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Aged Brie"));
+        Assert.That(items[0].SellIn, Is.EqualTo(9));
+        Assert.That(items[0].Quality, Is.EqualTo(21));
+    }
+
+    [Test]
+    public void UpdateQuality_WithAgedBrieWhenNegativeSellIn_ShouldIncreaseQualityTwice()
+    {
+        var items = new List<Item> { new() { Name = "Aged Brie", SellIn = 0, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Aged Brie"));
+        Assert.That(items[0].SellIn, Is.EqualTo(-1));
+        Assert.That(items[0].Quality, Is.EqualTo(22));
+    }
+
+    [Test]
+    public void UpdateQuality_WithAgedBrie_ShouldNotIncreaseQualityBeyond50()
+    {
+        var items = new List<Item> { new() { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Aged Brie"));
+        Assert.That(items[0].SellIn, Is.EqualTo(9));
+        Assert.That(items[0].Quality, Is.EqualTo(50));
+    }
+
+    [Test]
+    public void UpdateQuality_WithSulfuras_ShouldDoNothing()
+    {
+        var items = new List<Item> { new() { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Sulfuras, Hand of Ragnaros"));
+        Assert.That(items[0].SellIn, Is.EqualTo(10));
+        Assert.That(items[0].Quality, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void UpdateQuality_WithBackstagePassesWhenSellIs11_ShouldIncreaseQualityOnce()
+    {
+        var items = new List<Item>
+            { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Backstage passes to a TAFKAL80ETC concert"));
+        Assert.That(items[0].SellIn, Is.EqualTo(10));
+        Assert.That(items[0].Quality, Is.EqualTo(21));
+    }
+
+    [TestCase(10)]
+    [TestCase(9)]
+    [TestCase(8)]
+    [TestCase(7)]
+    [TestCase(6)]
+    public void UpdateQuality_WithBackstagePassesWhenSellInBetween6And10_ShouldIncreaseQualityTwice(int sellIn)
+    {
+        var items = new List<Item>
+            { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Backstage passes to a TAFKAL80ETC concert"));
+        Assert.That(items[0].SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(items[0].Quality, Is.EqualTo(22));
+    }
+
+    [TestCase(5)]
+    [TestCase(4)]
+    [TestCase(3)]
+    [TestCase(2)]
+    [TestCase(1)]
+    public void UpdateQuality_WithBackstagePassesWhenSellInBetween0And5_ShouldIncreaseQualityTwice(int sellIn)
+    {
+        var items = new List<Item>
+            { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Backstage passes to a TAFKAL80ETC concert"));
+        Assert.That(items[0].SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(items[0].Quality, Is.EqualTo(23));
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    public void UpdateQuality_WithBackstagePassesWhenSellInIs0AndBelow_ShouldSetQualityTo0(int sellIn)
+    {
+        var items = new List<Item>
+            { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 20 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].Name, Is.EqualTo("Backstage passes to a TAFKAL80ETC concert"));
+        Assert.That(items[0].SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
+}
